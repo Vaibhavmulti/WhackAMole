664c4f71e7240dae26b4278e8db4dc7295529fa7
Vaibhavmulti <vaibhavmulti@gmail.com>
2019-03-09 09:55:38 +0530

    Multiple levels

---
+++ Assets/Scripts/ScoreAndTime1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreAndTime1 : MonoBehaviour {
+    private GameControllerMod gamecontroller;
+
+    public TextMesh textMesh;
+	// Use this for initialization
+	void Start () {
+        gamecontroller = GameObject.FindObjectOfType<GameControllerMod>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        textMesh.text = "         Hit em' all!! \n\n" + "  Time :" +Mathf.Floor(gamecontroller.checkGameTimer) +"\n  Score :" +DetectAMole.score;
+	}
+}
---
+++ Assets/Scripts/ScoreAndTime2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreAndTime2 : MonoBehaviour {
+    private GameControllerHard gamecontroller;
+
+    public TextMesh textMesh;
+	// Use this for initialization
+	void Start () {
+        gamecontroller = GameObject.FindObjectOfType<GameControllerHard>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        textMesh.text = "         Hit em' all!! \n\n" + "  Time :" +Mathf.Floor(gamecontroller.checkGameTimer) +"\n  Score :" +DetectAMole.score;
+	}
+}
